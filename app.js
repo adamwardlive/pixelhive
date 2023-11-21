a7f78b3e6058893d34f4bca75ad9c119b1347d1b
adam ward <125765973+adamwardlive@users.noreply.github.com>
2023-11-21 19:29:17 +0000

    Update app.js
    added fetchphotos function to app.js

--- app.js
+++ app.js
@@ -28,24 +28,32 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
   
-  function fetchPhotos() {
-    fetch('/api/photographs')
+// Function to fetch photos from another Logic App endpoint
+function fetchPhotos() {
+  fetch('https://prod-59.northeurope.logic.azure.com:443/workflows/a0e1c136c0c145c08f6a5f4edab4a99b/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=XoEch95vh_XunmD0scYJY7-RzTLPiG4u15E2iDKsjYI')
     .then(response => response.json())
     .then(photos => {
       const gallery = document.getElementById('photo-gallery');
-      gallery.innerHTML = ''; // Clear gallery
+      gallery.innerHTML = ''; // Clear gallery before adding new photos
       photos.forEach(photo => {
         const photoItem = document.createElement('div');
-        photoItem.classList.add('photo-item');
-        photoItem.innerHTML = `
-          <img src="${photo.filePath}" alt="${photo.caption}" title="${photo.caption}">
-          <p>${photo.caption}</p>
-        `;
+        photoItem.className = 'photo-item';
+        const image = document.createElement('img');
+        image.src = photo.url; // Assuming 'url' is the property containing the photo URL
+        image.alt = photo.caption; // Assuming 'caption' is the property containing the caption
+        const caption = document.createElement('p');
+        caption.textContent = photo.caption;
+        photoItem.appendChild(image);
+        photoItem.appendChild(caption);
         gallery.appendChild(photoItem);
       });
     })
-    .catch((error) => {
+    .catch(error => {
       console.error('Error:', error);
     });
-  }
+}
+
+// Don't forget to call fetchPhotos to actually execute the function on page load or after photo uploads
+fetchPhotos();
+
   
