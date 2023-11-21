7c7563a97875a53bfab706b58ceca6b26438026d
adam ward <125765973+adamwardlive@users.noreply.github.com>
2023-11-21 19:39:54 +0000

    Update app.js

--- app.js
+++ app.js
@@ -28,46 +28,37 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
   
-// Function to fetch photos from another Logic App endpoint
 function fetchPhotos() {
   fetch('https://prod-59.northeurope.logic.azure.com:443/workflows/a0e1c136c0c145c08f6a5f4edab4a99b/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=XoEch95vh_XunmD0scYJY7-RzTLPiG4u15E2iDKsjYI')
     .then(response => response.json())
     .then(photos => {
       const gallery = document.getElementById('photo-gallery');
-      gallery.innerHTML = ''; // Clear the gallery before adding new photos.
+      gallery.innerHTML = ''; // Clear gallery before adding new photos
       
-      // Ensure 'photos' is actually an array and has items.
-      if (Array.isArray(photos) && photos.length > 0) {
-        photos.forEach(photo => {
-          const photoItem = document.createElement('div');
-          photoItem.className = 'photo-item';
-          
-          // Check if the photo object has the 'url' and 'caption' properties.
-          if(photo.url && photo.caption) {
-            const image = document.createElement('img');
-            image.src = photo.url; // Use the actual property name for the photo URL.
-            image.alt = photo.caption; // Use the actual property name for the caption.
-            
-            const caption = document.createElement('p');
-            caption.textContent = photo.caption; // Use the actual property name for the caption.
-            
-            photoItem.appendChild(image);
-            photoItem.appendChild(caption);
-            gallery.appendChild(photoItem);
-          } else {
-            // If there's no 'url' or 'caption', log an error or handle it appropriately.
-            console.error('Photo object is missing the url or caption property', photo);
-          }
-        });
-      } else {
-        // Handle the case where 'photos' is not an array or is empty.
-        console.error('Photos data is not an array or is empty', photos);
-      }
+      photos.forEach(photo => {
+        const photoItem = document.createElement('div');
+        photoItem.className = 'photo-item';
+        
+        // Assuming 'fileLocation' is the property that contains the URL of the photo
+        const image = document.createElement('img');
+        image.src = photo.fileLocation; 
+        image.alt = 'Photo'; // There's no caption property, so we use a generic alt text
+        
+        // Assuming 'fileName' is the property you want to use as a caption
+        const caption = document.createElement('p');
+        caption.textContent = photo.fileName;
+        
+        photoItem.appendChild(image);
+        photoItem.appendChild(caption);
+        gallery.appendChild(photoItem);
+      });
     })
     .catch(error => {
       console.error('Error fetching photos:', error);
     });
 }
+
+
 
 
 // Don't forget to call fetchPhotos to actually execute the function on page load or after photo uploads
