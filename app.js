84de2d5f1f7720ddeb59074283d2addd481ef059
adam ward <125765973+adamwardlive@users.noreply.github.com>
2023-11-27 20:31:43 +0000

    Update app.js

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ document.addEventListener('DOMContentLoaded', function() {
   function uploadPhoto() {
     var formData = new FormData();
     var photoInput = document.getElementById('photo-upload');
-    formData.append('file', photoInput.files[0]); // 'file' is the expected key for the file in the Logic App
+    formData.append('file', photoInput.files[0]);
 
-    var uploadUrl = 'https://prod-05.northeurope.logic.azure.com:443/workflows/d16fda7f5e084f4abf6574b81d11e46d/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=VOco_-4vvar98WT0aX7jJL2SviHzsybZzbTo9F_cJUk'; // Replace with your actual Logic App URL
+    var uploadUrl = 'https://prod-05.northeurope.logic.azure.com:443/workflows/d16fda7f5e084f4abf6574b81d11e46d/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=VOco_-4vvar98WT0aX7jJL2SviHzsybZzbTo9F_cJUk';
 
     fetch(uploadUrl, {
       method: 'POST',
@@ -32,7 +32,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function fetchAndDisplayImages() {
     const galleryElement = document.getElementById('gallery');
-    const logicAppUrl = 'https://prod-14.northeurope.logic.azure.com:443/workflows/d1796592c81d4d238f1f9462b580ec50/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=kcVG7x0CvW9ugWmDkWUcUibkrcA83Gs2h8Q3qE5V5EI'; // Replace with the URL to fetch images
+    const logicAppUrl = 'https://prod-14.northeurope.logic.azure.com:443/workflows/d1796592c81d4d238f1f9462b580ec50/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=kcVG7x0CvW9ugWmDkWUcUibkrcA83Gs2h8Q3qE5V5EI';
 
     fetch(logicAppUrl)
     .then(response => {
@@ -43,14 +43,12 @@ document.addEventListener('DOMContentLoaded', function() {
     })
     .then(data => {
       galleryElement.innerHTML = ''; // Clear existing gallery items
-
       data.forEach(item => {
-        const imageUrl = constructImageUrl(item); // Implement this function based on how you get your URLs
+        const imageUrl = item.filePath; // Assuming the filePath is the URL or path to the image
         const imageElement = document.createElement('img');
         imageElement.src = imageUrl;
-        imageElement.alt = item.caption;
+        imageElement.alt = item.caption || 'Uploaded Image'; // Use caption if available
         imageElement.classList.add('gallery-image');
-
         galleryElement.appendChild(imageElement);
       });
     })
@@ -59,7 +57,8 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
-  // Call fetchAndDisplayImages when the page loads to display existing images
+  // Initial call to populate the gallery
   fetchAndDisplayImages();
-}); 
+});
+
 
