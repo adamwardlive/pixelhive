b58867320fbb1257f8f718316069803fb5a50a1f
adam ward <125765973+adamwardlive@users.noreply.github.com>
2023-11-27 20:36:49 +0000

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
   function uploadPhoto() {
     var formData = new FormData();
     var photoInput = document.getElementById('photo-upload');
-    formData.append('file', photoInput.files[0]);
+    formData.append('file', photoInput.files[0]); // Ensure this matches the name expected by the server
 
     var uploadUrl = 'https://prod-05.northeurope.logic.azure.com:443/workflows/d16fda7f5e084f4abf6574b81d11e46d/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=VOco_-4vvar98WT0aX7jJL2SviHzsybZzbTo9F_cJUk';
 
@@ -33,6 +33,7 @@ document.addEventListener('DOMContentLoaded', function() {
   function fetchAndDisplayImages() {
     const galleryElement = document.getElementById('gallery');
     const logicAppUrl = 'https://prod-14.northeurope.logic.azure.com:443/workflows/d1796592c81d4d238f1f9462b580ec50/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=kcVG7x0CvW9ugWmDkWUcUibkrcA83Gs2h8Q3qE5V5EI';
+    const blobStorageBaseUrl = 'https://pixelhiveb00787643.blob.core.windows.net/pixelhive-img-share-b00787643/';
 
     fetch(logicAppUrl)
     .then(response => {
@@ -44,10 +45,10 @@ document.addEventListener('DOMContentLoaded', function() {
     .then(data => {
       galleryElement.innerHTML = ''; // Clear existing gallery items
       data.forEach(item => {
-        const imageUrl = item.filePath; // Assuming the filePath is the URL or path to the image
+        const imageUrl = blobStorageBaseUrl + item.filePath; // Construct the full URL
         const imageElement = document.createElement('img');
         imageElement.src = imageUrl;
-        imageElement.alt = item.caption || 'Uploaded Image'; // Use caption if available
+        imageElement.alt = item.caption || 'Uploaded Image';
         imageElement.classList.add('gallery-image');
         galleryElement.appendChild(imageElement);
       });
@@ -60,5 +61,6 @@ document.addEventListener('DOMContentLoaded', function() {
   // Initial call to populate the gallery
   fetchAndDisplayImages();
 });
+
 
 
