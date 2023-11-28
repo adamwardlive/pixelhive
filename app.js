f950a1c41dfdefff908814a07872c8b177687d55
adam ward <125765973+adamwardlive@users.noreply.github.com>
2023-11-28 21:18:38 +0000

    Update app.js

--- app.js
+++ app.js
@@ -11,11 +11,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function uploadPhoto() {
     var formData = new FormData();
-    var photoInput = document.getElementById('photo-upload'); 
-    formData.append('file', photoInput.files[0]); 
+    var photoInput = document.getElementById('photo-upload'); // Make sure this is the ID of your file input
+    formData.append('file', photoInput.files[0]); // 'file' is the expected key for the file in the Logic App
 
-    var uploadUrl = 'https://prod-05.northeurope.logic.azure.com:443/workflows/d16fda7f5e084f4abf6574b81d11e46d/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=VOco_-4vvar98WT0aX7jJL2SviHzsybZzbTo9F_cJUk'; 
+    var uploadUrl = 'https://prod-05.northeurope.logic.azure.com:443/workflows/d16fda7f5e084f4abf6574b81d11e46d/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=VOco_-4vvar98WT0aX7jJL2SviHzsybZzbTo9F_cJUk'; // Replace with your actual Logic App URL
 
+    // No need to explicitly set 'Content-Type' header for 'multipart/form-data', 
+    // the browser will automatically set it along with the boundary parameter
     fetch(uploadUrl, {
       method: 'POST',
       body: formData
@@ -28,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function() {
     })
     .then(data => {
       console.log('Success:', data);
-      window.location.reload(); 
+      window.location.reload(); // This will refresh the page
     })
     .catch(error => {
       console.error('Error during upload:', error);
@@ -38,6 +40,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   function fetchAndDisplayImages() {
     const blobStorageBaseUrl = 'https://pixelhiveb00787643.blob.core.windows.net/pixelhive-img-share-b00787643';
+  
     const logicAppUrl = 'https://prod-14.northeurope.logic.azure.com:443/workflows/d1796592c81d4d238f1f9462b580ec50/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=kcVG7x0CvW9ugWmDkWUcUibkrcA83Gs2h8Q3qE5V5EI';
   
     fetch(logicAppUrl)
@@ -56,39 +59,21 @@ document.addEventListener('DOMContentLoaded', function() {
   
         galleryElement.innerHTML = '';
   
-        data.sort((a, b) => b._ts - a._ts);
+        data.sort((a, b) => b._ts - a._ts); // Sort based on a timestamp field
   
         data.forEach(item => {
           const filePath = item.filePath.startsWith('/pixelhive-img-share-b00787643/') ? item.filePath.split('/pixelhive-img-share-b00787643/')[1] : item.filePath;
           const imageUrl = `${blobStorageBaseUrl}/${filePath}`;
   
-          const imgContainer = document.createElement('div');
-          imgContainer.classList.add('image-container');
-  
+          const linkElement = document.createElement('a');
+          linkElement.href = `image-detail.html?image=${encodeURIComponent(imageUrl)}&name=${encodeURIComponent(item.fileName)}&timestamp=${encodeURIComponent(item._ts)}&caption=${encodeURIComponent(item.caption)}`;  
           const imgElement = document.createElement('img');
           imgElement.src = imageUrl;
           imgElement.alt = item.fileName;
           imgElement.classList.add('gallery-image');
   
-          imgContainer.appendChild(imgElement);
-  
-          const updateButton = document.createElement('button');
-          updateButton.innerText = 'Update';
-          updateButton.addEventListener('click', function(event) {
-            event.stopPropagation();
-            showUpdateForm(item);
-          });
-          imgContainer.appendChild(updateButton);
-  
-          const deleteButton = document.createElement('button');
-          deleteButton.innerText = 'Delete';
-          deleteButton.addEventListener('click', function(event) {
-            event.stopPropagation();
-            deleteImage(item.id);
-          });
-          imgContainer.appendChild(deleteButton);
-  
-          galleryElement.appendChild(imgContainer);
+          linkElement.appendChild(imgElement);
+          galleryElement.appendChild(linkElement);
         });
       })
       .catch(error => {
@@ -96,83 +81,16 @@ document.addEventListener('DOMContentLoaded', function() {
       });
   }
   
-
-  function showUpdateForm(item) {
-    // Assuming 'id', 'caption' are properties of the item
-    const updateForm = document.getElementById('update-form');
-    document.getElementById('update-photo-id').value = item.id;
-    document.getElementById('update-photo-caption').value = item.caption;
-  
-    updateForm.onsubmit = function(e) {
-      e.preventDefault();
-      const updatedCaption = document.getElementById('update-photo-caption').value;
-  
-      // Replace with your actual Logic App URL for update
-      const updateUrl = 'https://your-logic-app-url-for-update';
-  
-      fetch(updateUrl, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ id: item.id, caption: updatedCaption })
-      })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log('Update successful:', data);
-        // Action after successful update (e.g., refresh the gallery)
-        fetchAndDisplayImages();
-      })
-      .catch(error => {
-        console.error('Error during update:', error);
-      });
-    };
-  
-    // Show the update form
-    document.getElementById('update-section').style.display = 'block';
-  }
-  
-
-  function deleteImage(imageId) {
-    // Confirm before deleting
-    if (!confirm("Are you sure you want to delete this image?")) {
-      return;
-    }
-  
-    // Replace with your actual Logic App URL for delete
-    const deleteUrl = 'https://your-logic-app-url-for-delete';
-  
-    fetch(deleteUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ id: imageId })
-    })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log('Delete successful:', data);
-      // Action after successful delete (e.g., refresh the gallery)
-      fetchAndDisplayImages();
-    })
-    .catch(error => {
-      console.error('Error during delete:', error);
-    });
-  }
-  
-
+  // Call the function to fetch and display images
   fetchAndDisplayImages();  
 });
+
+
+
+
+
+
+
 
 
 
